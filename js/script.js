261cc05d505ea5f7056ae614caccb33d07778b50
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-24 14:55:50 +0200

    add api dinamic cards

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const APIUri = `https://lanciweb.github.io/demo/api/pictures/`;
+
+const cardRow = document.getElementById("card-row");
+
+axios.get(APIUri).then((response) => {
+  console.log(response.data[1]);
+});
+for (let i = 0; i < 6; i++) {
+  axios.get(APIUri).then((response) => {
+    const currentCard = response.data[i];
+    cardRow.innerHTML += `
+                           <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card d-inline-block">
+                            <div class="card-pin"><img src="./img/pin.svg" alt=""></div>
+                            <div class="card-img-top p-4">
+                                <img src="${currentCard.url}" alt="">
+                            </div>
+                            <div class="card-body">
+                                <div class="card-text">
+                                    <div class="data-img">${currentCard.date}</div>
+                                    <h4 class="fw-bold">${currentCard.title}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+  });
+}
