390801769522ca56780ec2ff769053efbcb53798
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-24 15:35:33 +0200

    pixel perfect

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ const APIUri = `https://lanciweb.github.io/demo/api/pictures/`;
 
 const cardRow = document.getElementById("card-row");
 
-axios.get(APIUri).then((response) => {
-  console.log(response.data[1]);
-});
+axios.get(APIUri).then((response) => {});
 for (let i = 0; i < 6; i++) {
   axios.get(APIUri).then((response) => {
     const currentCard = response.data[i];
@@ -12,10 +10,10 @@ for (let i = 0; i < 6; i++) {
                            <div class="col-12 col-md-6 col-lg-4">
                         <div class="card d-inline-block">
                             <div class="card-pin"><img src="./img/pin.svg" alt=""></div>
-                            <div class="card-img-top p-4">
+                            <div class="card-img-top px-3 pt-3">
                                 <img src="${currentCard.url}" alt="">
                             </div>
-                            <div class="card-body">
+                            <div class="card-body py-1">
                                 <div class="card-text">
                                     <div class="data-img">${currentCard.date}</div>
                                     <h4 class="fw-bold">${currentCard.title}</h4>
@@ -23,5 +21,7 @@ for (let i = 0; i < 6; i++) {
                             </div>
                         </div>
                     </div>`;
-  });
+  }).catch(error =>{
+    alert(error.message);
+  })
 }
