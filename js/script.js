29f4b6cbee4c7c7d445a6526226334ab6cc0a1a0
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-25 20:12:30 +0200

    fix axios call

--- js/script.js
+++ js/script.js
@@ -2,29 +2,29 @@ const APIUri = `https://lanciweb.github.io/demo/api/pictures/`;
 
 const cardRow = document.getElementById("card-row");
 
-axios.get(APIUri).then((response) => {});
-for (let i = 0; i < 6; i++) {
-  axios.get(APIUri).then((response) => {
-    const currentCard = response.data[i];
-    cardRow.innerHTML += `
+axios.get(APIUri).then((response) => {
+  const currentCard = response.data;
+ 
+  currentCard.forEach((element) => {
+      cardRow.innerHTML += `
                            <div class="col-12 col-md-6 col-lg-4">
                         <div class="card d-inline-block">
                             <div class="card-pin"><img src="./img/pin.svg" alt=""></div>
                             <div class="card-img-top px-3 pt-3">
-                                <img src="${currentCard.url}" alt="">
+                                <img src="${element.url}" alt="">
                             </div>
                             <div class="card-body py-1">
                                 <div class="card-text">
-                                    <div class="data-img">${currentCard.date}</div>
-                                    <h4 class="fw-bold">${currentCard.title}</h4>
+                                    <div class="data-img">${element.date}</div>
+                                    <h2 class="fw-bold">${element.title}</h2>
                                 </div>
                             </div>
                         </div>
                     </div>`;
-  }).catch(error =>{
-    alert(error.message);
-  })
-};
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+});
 
-// card si inclina al passaggio del mouse
 
