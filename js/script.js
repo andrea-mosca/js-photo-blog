b9c760221b41e7c7159d4e3b852228b2d0a99eaf
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-28 12:57:53 +0200

    card url selection

--- js/script.js
+++ js/script.js
@@ -2,29 +2,43 @@ const APIUri = `https://lanciweb.github.io/demo/api/pictures/`;
 
 const cardRow = document.getElementById("card-row");
 
+const generateHTMLCard = (element) => {
+    const cardHTML = `
+    <div class="col-12 col-md-6 col-lg-4">
+        <div class="card d-inline-block">
+            <div class="card-pin"><img src="./img/pin.svg" alt=""></div>
+            <div class="card-img-top px-3 pt-3">
+                 <img src="${element.url}" alt="">
+            </div>
+            <div class="card-body py-1">
+                <div class="card-text">
+                    <div class="data-img">${element.date}</div>
+                    <h2 class="fw-bold">${element.title}</h2>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    return cardHTML;
+};
+
 axios.get(APIUri).then((response) => {
   const currentCard = response.data;
- 
-  currentCard.forEach((element) => {
-      cardRow.innerHTML += `
-                           <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card d-inline-block">
-                            <div class="card-pin"><img src="./img/pin.svg" alt=""></div>
-                            <div class="card-img-top px-3 pt-3">
-                                <img src="${element.url}" alt="">
-                            </div>
-                            <div class="card-body py-1">
-                                <div class="card-text">
-                                    <div class="data-img">${element.date}</div>
-                                    <h2 class="fw-bold">${element.title}</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>`;
-    })
-    .catch((error) => {
-      alert(error.message);
+    let cardsHTML = '';
+  currentCard
+    .forEach((element) => {
+      cardsHTML += generateHTMLCard(element);
     });
+    cardRow.innerHTML += cardsHTML;
+    const cardNodes = document.querySelectorAll(".card");
+    
+    cardNodes.forEach(cardNode => {
+        cardNode.addEventListener("click", ()=>{
+            const imageSrc = cardNode.querySelector('.card-img-top img').src;
+            console.log(imageSrc);
+    
+        });
+    });
+    
 });
 
 
